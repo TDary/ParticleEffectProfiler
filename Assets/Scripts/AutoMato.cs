6189f58d2cb2a328d40b42b49454bf5034cecb4f
chenderui <chenderui1@thewesthill.net>
2024-11-13 17:18:38 +0800

    优化一下 可运行

---
+++ Assets/Scripts/AutoMato.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Matory;
+using System;
+using UnityEditor.PackageManager;
+
+namespace Mator
+{
+    public class AutoMato : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        static Mato runner = null;
+        private bool swich = false;
+        void Awake()
+        {
+            if (runner == null)
+            {
+                runner = gameObject.AddComponent<Mato>();
+                try
+                {
+                    if (runner != null)
+                    {
+                        runner.Init();
+                        runner.m_Pro.AddMethod("gm",gmMethod);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.LogException(ex);
+                }
+            }
+        }
+
+        private object gmMethod(string ip, string[] args)
+        {
+            try
+            {
+                string command = args[0];
+                if (command == "FrameTargetRelease")
+                {
+                    Application.targetFrameRate = -1;
+                    QualitySettings.vSyncCount = 0;
+                }
+                else if (command == "ShowFps")
+                {
+                    var fpseSet = FindObjectOfType<Assets.Scripts.FPSCounter>();
+                    fpseSet.StartFps();
+                }
+                else if (command == "ShutUpFps")
+                {
+                    var fpseSet = FindObjectOfType<Assets.Scripts.FPSCounter>();
+                    fpseSet.StopFps();
+                }
+                else if(command == "PlayEffect")
+                {
+                    var ulodSwitch = FindObjectOfType<Assets.Scripts.MainController>();
+                    if (args[1] != "")
+                    {
+                        var input = GameObject.Find("EffectName");
+                        var texts = input.GetComponent<UnityEngine.UI.Text>();
+                        texts.text = args[1];
+                    }
+                    if (args[2] != "")
+                    {
+                        var input = GameObject.Find("count");
+                        var count = input.GetComponent<UnityEngine.UI.Text>();
+                        count.text = args[2];
+                    }
+                    ulodSwitch.Play();
+                }
+                else if (command == "TimeFrame")
+                {
+                    Debug.Log("当前帧号：" + Time.frameCount);
+                }
+                return "ok";
+            }
+            catch (Exception e)
+            {
+                return e.ToString();
+            }
+        }
+    }
+}
--- Assets/Scripts/MainController.cs
+++ Assets/Scripts/MainController.cs
@@ -1,8 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+#if UNITY_EDITOR
 using UnityEditor;
-using UnityEditor.PackageManager;
+#endif
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.VFX;
